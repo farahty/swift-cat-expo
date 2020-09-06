af741ca07caecf803ce87df776fbe19428751a1f
Nimer Farahty <nimer.farahty@gmail.com>
2020-09-06 15:06:44 +0300

    Initial Commit

---
+++ CatExpo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CatExpo
+//
+//  Created by Nimer Farahty on 9/6/20.
+//  Copyright © 2020 Nimer Farahty. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
